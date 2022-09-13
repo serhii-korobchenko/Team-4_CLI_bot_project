4fc978c314c602edab6a57f76dce62eca2534e59
Serhii Korobchenko <jchild2008@gmail.com>
2022-09-13 11:24:49 +0300

    130922

--- test_endless_typing.py
+++ test_endless_typing.py
@@ -6,6 +6,9 @@ command_list = ['hello','add','change', 'phone', 'show', 'good','close', 'exit',
            'sortfiles','guess','checkbirth','changeemail']
 
 
+main_list = []
+join_command_list = []
+
 def on_press(key):
     try:
         if key == keyboard.Key.esc:
@@ -15,37 +18,64 @@ def on_press(key):
         elif key == keyboard.Key.enter:
             print('')
             print(''.join(map(str, main_list)), end='\r', flush=True)
+            return False
+
+        elif key == keyboard.Key.shift:
+            pass
+
+        elif key == keyboard.Key.space:
+            main_list.append(' ')
+
+        elif key == keyboard.Key.backspace:
+
+            if len(main_list) >= 0:
+                main_list.pop()
+                print('                                                                                                            ', end='\r')
+                print(''.join(map(str, main_list)), end='\r', flush=True)
 
         else:
             main_list.append(key.char)
             for command in command_list:
-                #print("command[0]", command[0])
-                #print("main_list[0]", main_list[0].char)
+                              
                 
                 if len(main_list) == 1 and command[0] == main_list[0]:
             
-                    print(command, end='\t\t\t')
+                    join_command_list.append(command)
 
                 elif len(main_list) == 2 and command[0] == main_list[0] and command[1] == main_list[1]:
 
-                    print(command, end='\t\t\t')
+                    join_command_list.append(command)
 
                 elif len(main_list) == 3 and command[0] == main_list[0] and command[1] == main_list[1] and command[2] == main_list[2]:
 
-                    print(command, end='\t\t\t')
+                    join_command_list.append(command)
 
-            print(''.join(map(str, main_list)), end='\r', flush=True)
+                elif len(main_list) == 4 and command[0] == main_list[0] and command[1] == main_list[1] and command[2] == main_list[2] and command[3] == main_list[3]:
+
+                    join_command_list.append(command)
+
+            print('                                                                                                            ', end='\r')
+            print(''.join(map(str, main_list)), end='\r\t\t\t\t', flush=True)
+            print(', '.join(map(str, join_command_list)), end='\r', flush=True)
+            join_command_list.clear()
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
 
-main_list = []
+    except IndexError:
+        print('', end='')
 
-# Collect events until released
-with keyboard.Listener(on_press=on_press) as listener:   #on_release=on_release
-    listener.join()
+def main_guess():
+
+
+    # Collect events until released
+    with keyboard.Listener(on_press=on_press) as listener:   #on_release=on_release
+        listener.join()
+        
+    # ...or, in a non-blocking fashion:
+    listener = keyboard.Listener(on_press=on_press)   #on_release=on_release
+    listener.start()
+
+if __name__ == '__main__':
     
-
-# ...or, in a non-blocking fashion:
-listener = keyboard.Listener(on_press=on_press)   #on_release=on_release
-listener.start()
+    main_guess()
