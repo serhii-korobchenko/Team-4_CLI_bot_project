1716113524b9ca9b32af6288b14637fdabbfc7af
Serhii Korobchenko <jchild2008@gmail.com>
2022-09-14 20:21:09 +0300

    140922

--- Pekemons_IT_CLI_bot.py
+++ Pekemons_IT_CLI_bot.py
@@ -137,9 +137,9 @@ from datetime import datetime
 import pickle
 from copy import copy, deepcopy
 import csv
-from endless_typing import main_guess
-from sort_files import main_sort_files
 from pathlib import Path
+from pynput import keyboard
+import shutil
 
 
 # GLOBALS
@@ -919,6 +919,215 @@ def list_string(list):  # servise function for lookup function
 def good_buy_func():
     print('Good bye!')
     return 'stop'
+
+# ENDLESS TYPING
+def on_press(key):
+    
+    
+    global main_list_guess
+    global join_command_list
+    
+    try:
+        if key == keyboard.Key.esc:
+        
+        # Stop listener
+            return False
+
+        elif key == keyboard.Key.enter:
+            #print('')
+            #print(''.join(map(str, main_list_guess)), end='\r', flush=True)
+            return False
+
+            
+
+        elif key == keyboard.Key.shift:
+            pass
+
+        elif key == keyboard.Key.space:
+            main_list_guess.append(' ')
+
+        elif key == keyboard.Key.backspace:
+
+            if len(main_list_guess) >= 0:
+                main_list_guess.pop()
+                print('                                                                                     ', end='\r')
+                print(''.join(map(str, main_list_guess)), end='\r', flush=True)
+
+        else:
+            main_list_guess.append(key.char)
+            for command in command_list:
+                                              
+                if len(main_list_guess) == 1 and command[0] == main_list_guess[0]:
+            
+                    join_command_list.append(command)
+
+                elif len(main_list_guess) == 2 and command[0] == main_list_guess[0] and command[1] == main_list_guess[1]:
+
+                    join_command_list.append(command)
+
+                elif len(main_list_guess) == 3 and command[0] == main_list_guess[0] and command[1] == main_list_guess[1] and command[2] == main_list_guess[2]:
+
+                    join_command_list.append(command)
+
+                elif len(main_list_guess) == 4 and command[0] == main_list_guess[0] and command[1] == main_list_guess[1] and command[2] == main_list_guess[2] and command[3] == main_list_guess[3]:
+
+                    join_command_list.append(command)
+
+            print('                                                                                    ', end='\r')
+            print(''.join(map(str, main_list_guess)), end='\r\t\t\t\t', flush=True)
+            print(', '.join(map(str, join_command_list)), end='\r', flush=True)
+            join_command_list.clear()
+
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+    except IndexError:
+        print('', end='')
+
+def main_guess(command_list):
+    
+    global main_list_guess
+    global join_command_list
+   
+    main_list_guess = []
+    join_command_list = []
+
+
+    # Collect events until released
+    with keyboard.Listener(on_press=on_press) as listener:   #on_release=on_release
+        listener.join()
+        
+    # ...or, in a non-blocking fashion:
+    listener = keyboard.Listener(on_press=on_press)   #on_release=on_release
+    listener.start()
+    listener.stop()
+
+    return ''.join(map(str, main_list_guess))
+
+
+# SORT FILES
+
+### NORMILIZE
+CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
+
+TRANS = {}
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+
+def normalize(name: str):
+    t_name = name.translate(TRANS)
+    t_name = re.sub(r'\W', '_', t_name)
+    return t_name
+
+### FILES PARSER
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+MP3_AUDIO = []
+MY_OTHER = []
+ARCHIVES = []
+
+REGISTER_EXTENSIONS = {
+    'JPEG': JPEG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'MP3': MP3_AUDIO,
+    'ZIP': ARCHIVES
+}
+
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+
+def get_extension(filename: str):
+    return Path(filename).suffix[1:].upper()
+
+
+def scan(folder: Path) :
+    for item in folder.iterdir():
+        if item.is_dir():
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                scan(item)
+            continue
+
+        ext = get_extension(item.name)  
+        fullname = folder / item.name  
+        if not ext: 
+            MY_OTHER.append(fullname)
+        else:
+            try:
+                container = REGISTER_EXTENSIONS[ext]
+                EXTENSIONS.add(ext)
+                container.append(fullname)
+            except KeyError:
+                UNKNOWN.add(ext)
+                MY_OTHER.append(fullname)
+
+
+
+
+
+
+
+def handle_media(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    filename.replace(target_folder / normalize(filename.name))
+
+
+def handle_other(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    filename.replace(target_folder / normalize(filename.name))
+
+
+def handle_archive(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    folder_for_file = target_folder / normalize(filename.name.replace(filename.suffix, ''))
+
+    folder_for_file.mkdir(exist_ok=True, parents=True)
+    try:
+        shutil.unpack_archive(str(filename.resolve()),
+                              str(folder_for_file.resolve()))
+    except shutil.ReadError:
+        print(f'This is not archive {filename}!')
+        folder_for_file.rmdir()
+        return None
+    filename.unlink()
+
+
+def handle_folder(folder: Path):
+    try:
+        folder.rmdir()
+    except OSError:
+        print(f'Error deleted {folder}')
+
+
+def main_sort_files(folder: Path):
+    scan(folder)
+    for file in JPEG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPEG')
+    for file in JPG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPG')
+    for file in PNG_IMAGES:
+        handle_media(file, folder / 'images' / 'PNG')
+    for file in SVG_IMAGES:
+        handle_media(file, folder / 'images' / 'SVG')
+    for file in MP3_AUDIO:
+        handle_media(file, folder / 'audio' / 'MP3')
+
+    for file in MY_OTHER:
+        handle_other(file, folder / 'MY_OTHER')
+    for file in ARCHIVES:
+        handle_archive(file, folder / 'archives')
+
+    for folder in FOLDERS[::-1]:
+        handle_folder(folder)
 
 # MAIN BODY FUNCTION
 
